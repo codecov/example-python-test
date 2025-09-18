0ed6cec405bb6d3a3a492b089ce706a57561cf16
joseph-sentry <joseph.sawaya@sentry.io>
2025-09-18 19:05:39 -0400

    joey do it

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -1,17 +1,21 @@
 import random
 import time
+
 from .calculator import Calculator
 
 
 def test_flaky():
     assert random.random() <= 0.8
 
+
 def test_slow():
-    time.sleep(random.random() * 11)
+    time.sleep(random.random() * 10)
     assert True is True
+
 
 def test_fail():
     assert True is False
+
 
 def test_add():
     assert Calculator.add(1, 2) == 3.0
@@ -19,6 +23,7 @@ def test_add():
     assert Calculator.add(0, 2.0) == 2.0
     assert Calculator.add(2.0, 0) == 2.0
     assert Calculator.add(-4, 2.0) == -2.0
+
 
 def test_subtract():
     assert Calculator.subtract(1, 2) == -1.0
@@ -28,12 +33,14 @@ def test_subtract():
     assert Calculator.subtract(2.0, 0.0) == 2.0
     assert Calculator.subtract(-4, 2.0) == -6.0
 
+
 def test_multiply():
     assert Calculator.multiply(1, 2) == 2.0
     assert Calculator.multiply(1.0, 2.0) == 2.0
     assert Calculator.multiply(0, 2.0) == 0.0
     assert Calculator.multiply(2.0, 0.0) == 0.0
     assert Calculator.multiply(-4, 2.0) == -8.0
+
 
 def test_divide():
     # assert Calculator.divide(1, 2) == 0.5
